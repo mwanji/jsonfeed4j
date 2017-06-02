a0ac0d9d2dcd0df2fad4c39d7798bab2b902b774
Moandji Ezana <mwanji@gmail.com>
2017-06-02 23:15:40 +0200

    Read all plain top-level properties

--- src/main/java/jsonfeed4j/JsonFeed.java
+++ src/main/java/jsonfeed4j/JsonFeed.java
@@ -1,12 +1,21 @@
 package jsonfeed4j;
 
 import java.util.List;
+import java.util.function.BooleanSupplier;
 
 public class JsonFeed {
   
   private Versions version;
   private String title;
   private List<Item> items;
+  private String homePageUrl;
+  private String feedUrl;
+  private String description;
+  private String userComment;
+  private String nextUrl;
+  private String icon;
+  private String favicon;
+  private boolean expired;
 
   public Versions getVersion() {
     return version;
@@ -20,4 +29,35 @@ public class JsonFeed {
     return items;
   }
 
+  public String getHomePageUrl() {
+    return homePageUrl;
+  }
+
+  public String getFeedUrl() {
+    return feedUrl;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getUserComment() {
+    return userComment;
+  }
+
+  public String getNextUrl() {
+    return nextUrl;
+  }
+  
+  public String getIcon() {
+    return icon;
+  }
+
+  public String getFavicon() {
+    return favicon;
+  }
+
+  public boolean isExpired() {
+    return expired;
+  }
 }
--- src/test/java/jsonfeed4j/testutils/GsonJsonFeedReader.java
+++ src/test/java/jsonfeed4j/testutils/GsonJsonFeedReader.java
@@ -6,6 +6,7 @@ import java.io.Reader;
 import java.lang.reflect.Type;
 import java.nio.file.Path;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
@@ -19,7 +20,10 @@ import jsonfeed4j.Versions;
 
 public class GsonJsonFeedReader implements JsonFeedReader {
   
-  private final Gson gson = new GsonBuilder().registerTypeAdapter(Versions.class, new VersionsDeserializer()).create();
+  private final Gson gson = new GsonBuilder()
+      .registerTypeAdapter(Versions.class, new VersionsDeserializer())
+      .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+      .create();
   
   @Override
   public JsonFeed read(String jsonFeed) {

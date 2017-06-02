05c17cf8e523d021e7933d669de333c89e894ba2
Moandji Ezana <mwanji@gmail.com>
2017-06-03 01:38:11 +0200

    Add minimal Item properties

--- src/main/java/jsonfeed4j/Item.java
+++ src/main/java/jsonfeed4j/Item.java
@@ -2,4 +2,13 @@ package jsonfeed4j;
 
 public class Item {
 
+  private String id;
+  private String contentText;
+  
+  public String getId() {
+    return id;
+  }
+  public String getContentText() {
+    return contentText;
+  }
 }

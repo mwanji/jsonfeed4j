a2662090e5837e5d33f97453771571347d39f5ca
Moandji Ezana <mwanji@gmail.com>
2017-06-03 03:02:43 +0200

    Remove some read methods from JsonFeedReader

--- src/main/java/jsonfeed4j/JsonFeedReader.java
+++ src/main/java/jsonfeed4j/JsonFeedReader.java
@@ -1,13 +1,9 @@
 package jsonfeed4j;
 
-import java.io.InputStream;
 import java.io.Reader;
-import java.nio.file.Path;
 
 public interface JsonFeedReader {
 
   JsonFeed read(String string);
-  JsonFeed read(Path path);
-  JsonFeed read(InputStream inputStream);
   JsonFeed read(Reader reader);
 }

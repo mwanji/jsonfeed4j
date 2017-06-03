5fa44e549e7618f11494f3cb73d4827777937b54
Moandji Ezana <mwanji@gmail.com>
2017-06-03 02:28:41 +0200

    Add extensions to Item

--- src/main/java/jsonfeed4j/Item.java
+++ src/main/java/jsonfeed4j/Item.java
@@ -1,14 +1,23 @@
 package jsonfeed4j;
 
+import java.util.Collections;
+import java.util.Map;
+
 public class Item {
 
   private String id;
   private String contentText;
+  private Map<String, Object> extensions = Collections.emptyMap();
   
   public String getId() {
     return id;
   }
+  
   public String getContentText() {
     return contentText;
+  }
+
+  public Extensions getExtensions() {
+    return new Extensions(extensions);
   }
 }

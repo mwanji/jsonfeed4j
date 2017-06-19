072fba9aa9f72de13dd0e8135f0de2ec96594987
Moandji Ezana <mwanji@gmail.com>
2017-06-19 10:43:14 +0200

    Add documentation to JsonFeedReader

--- src/main/java/jsonfeed4j/JsonFeedReader.java
+++ src/main/java/jsonfeed4j/JsonFeedReader.java
@@ -2,6 +2,9 @@ package jsonfeed4j;
 
 import java.io.Reader;
 
+/**
+ * Converts input to a JsonFeed model. As jsonfeed4j's core does not depend on a JSON library, implementations are expected to bind to a specific library and provide configuration options.
+ **/
 public interface JsonFeedReader {
 
   JsonFeed read(String string);
